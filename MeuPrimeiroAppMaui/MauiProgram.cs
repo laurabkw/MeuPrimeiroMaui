454e1b7eedff2a71dc83ff1c459a33cc2c154316
Guilherme Schardong <gui.schardong@gmail.com>
2023-06-02 21:57:14 -0300

    primeiro commit

---
+++ MeuPrimeiroAppMaui/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MeuPrimeiroAppMaui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Roboto-Black.ttf", "Robotoblack");
+				fonts.AddFont("Roboto-Bold.ttf", "Robotobold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
